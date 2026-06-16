38a65c5e7cb7a979bb3972f189d19386f0145f4a
Lee JaeHyun <myleje@gmail.com>
2026-06-16 10:52:12 +0900

    feat: implement asynchronous server connection in ChatClient

--- TCPChatClient/ChatClient.cs
+++ TCPChatClient/ChatClient.cs
@@ -1,4 +1,5 @@
 using System.Net.Sockets;
+using System.Text;
 
 namespace TCPChatClient;
 
@@ -23,8 +24,56 @@ public class ChatClient : IDisposable
     // 연결 여부 프로퍼티
     public bool IsConnected => _isConnected && !_isDisposed;
     
+    // 생성자
+    public ChatClient(string serverIp, int serverPort)
+    {
+        _serverIp = serverIp;
+        _serverPort = serverPort;
+
+        _isConnected = false;
+        _isDisposed = false;
+    }
+
+    #region 서버 연결
+    public async Task ConnectServerAsync()
+    {
+        if (IsConnected)
+        {
+            Console.WriteLine("이미 서버에 연결되었습니다.");
+            return;
+        }
+
+        try
+        {
+            Console.WriteLine($"{_serverIp}:{_serverPort} 서버 연결 중 ...");
+            
+            // TcpClient 생성 및 서버 연결
+            _tcpClient = new TcpClient();
+            await _tcpClient.ConnectAsync(_serverIp, _serverPort);
+            
+            // 스트림 생성
+            _stream = _tcpClient.GetStream();
+            _reader = new StreamReader(_stream, Encoding.UTF8);
+            _writer = new StreamWriter(_stream, Encoding.UTF8) { AutoFlush = true };
+            
+            _isConnected = true;
+            Console.WriteLine("서버에 연결되었습니다.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"서버 연결 실패 {e.Message}");
+            throw;
+        }
+    }
+    #endregion
+
+    #region 리소스 해제
     public void Dispose()
     {
-        // TODO release managed resources here
+        _stream?.Dispose();
+        _reader?.Dispose();
+        _writer?.Dispose();
+        _tcpClient?.Dispose();
     }
+    #endregion
 }
