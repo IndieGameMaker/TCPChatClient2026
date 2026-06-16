3a7cfe051286686fae427c2fc04188b3e0b5c689
Lee JaeHyun <myleje@gmail.com>
2026-06-16 10:40:40 +0900

    feat: add connection management properties to ChatClient

--- TCPChatClient/ChatClient.cs
+++ TCPChatClient/ChatClient.cs
@@ -11,6 +11,18 @@ public class ChatClient : IDisposable
     private StreamReader? _reader;
     private StreamWriter? _writer;
     
+    // 연결 여부
+    private bool _isConnected;
+    // 리소스 해제여부
+    private bool _isDisposed;
+    
+    // 서버 IP, Port
+    private readonly string _serverIp;
+    private readonly int _serverPort;
+    
+    // 연결 여부 프로퍼티
+    public bool IsConnected => _isConnected && !_isDisposed;
+    
     public void Dispose()
     {
         // TODO release managed resources here
